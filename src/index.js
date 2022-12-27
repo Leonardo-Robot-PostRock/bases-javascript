b1b84de3b949f1d6ce3b8307f8066c25c3957a16
Leonardo Puebla <leonardo38361@gmail.com>
2022-12-26 22:34:43 -0300

    operadores ternarios

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-// const getImagenPromesa = () => new Promise((reslove) =>
-//     reslove('https://sdfadfsadfsd.com')
-// )
+const activo = true;
 
+// let mensaje = '';
 
-// getImagenPromesa().then(console.log)
+// if(activo) {
+//     mensaje = 'Activo'
+// }else{
+//     mensaje = 'Inactivo'
+// }
 
-getImage = async () => {
-    try {
-        const apiKey = 'CaLzFBSf9WESCfQPoiHahB9GipP88cGW';
-        const peticion = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-        const { data } = await peticion.json()
-
-        const { url } = data.images.original;
-        const img = document.createElement('img')
-        img.src = url;
-        document.body.append(img)
-        console.log('ver', data);
-    } catch (err) {
-        console.log(err);
-    }
-}
-getImage().then(console.log)
+// const mensaje = { activo } ? 'Activo' : 'Inactivo';
+// const mensaje = !{ activo } ? 'Activo' : null;
+const mensaje = !activo && 'Activo'
+console.log(mensaje);
